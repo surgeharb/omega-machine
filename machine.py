71b1c555e2e90dc4990e7fc47be57c415c09cef7
surgeharb <me@sergeharb.com>
2018-10-20 20:44:20 +0300

    implement microprogram instructions

--- machine.py
+++ machine.py
@@ -2,84 +2,228 @@ from config import *
 from program import *
 
 # Main Memory containing program instructions
-MAIN_MEMORY = code
+M = code
+
+# Program Counter
+PC = 0
 
 # Registers
-REG = [0, 0, 0]
+R = [0, 0, 0]
+
+# OFFSETS
+OP_OFFSET = 26
+R1_OFFSET = 21
+R2_OFFSET = 16
+R3_OFFSET = 11
 
 def printRegisters():
   print('==> REGISTERS PRINT START')
-  print('REG 1:', REG[0])
-  print('REG 2:', REG[1])
-  print('REG 3:', REG[2])
+  print('REG 1:', R[0])
+  print('REG 2:', R[1])
+  print('REG 3:', R[2])
   print('<== REGISTERS PRINT END\n')
 
-def instructionDecode(instruction):
-  print('==> INSTRUCTION FETCH & DECODE START')
-  print('instruction:', ''.join(str(x) for x in list(reversed(instruction))))
+def _load(destination, address):
+  R[destination] = M[address]
+  PC = PC + 1
 
-  opcode = ''.join(str(x) for x in list(reversed(instruction[26:32])))
-  reg1 = ''.join(str(x) for x in list(reversed(instruction[21:26])))
-  reg2 = ''.join(str(x) for x in list(reversed(instruction[16:21])))
-  reg3 = ''.join(str(x) for x in list(reversed(instruction[11:16])))
-  unused = ''.join(str(x) for x in list(reversed(instruction[0:11])))
+def _store(source, address):
+  M[address] = R[source]
+  PC = PC + 1
 
-  print('opcode:', opcode)
-  print('reg1:', reg1)
-  print('reg2:', reg2)
-  print('reg3:', reg3)
-  print('unused:', unused)
-  print('<== INSTRUCTION FETCH & DECODE END\n')
+def _add(destination, source1, source2):
+  R[destination]= R[source1] + R[source2]
+  PC = PC + 1
 
-  return {
-    'opcode': opcodes[opcode],
-    'param1': int(reg1, 2),
-    'param2': int(reg2, 2),
-    'param3': int(reg3, 2)
-  }
+def _sub(destination, source1, source2):
+  R[destination]= R[source1] - R[source2]
+  PC = PC + 1
 
-def instructionExec(instruction):
-  reg_index = instruction['param3'] - 1
-  param2 = ''
+def _beq(source1, source2, address):
+  if(R[source2] - R[source1] == 0):
+    PC = address
+  else:
+    PC = PC + 1
 
-  if (instruction['opcode'] == 'add'):
-    REG[reg_index] = REG[instruction['param1'] - 1] + REG[instruction['param2'] - 1]
-    param2 = 'R' + str(instruction['param2'])
+def _shl(destination, source1, source2):
+  R[destination] = R[source1] << R[source2]
+  PC = PC + 1
 
-  elif (instruction['opcode'] == 'sub'):
-    REG[reg_index] = REG[instruction['param1'] - 1] - REG[instruction['param2'] - 1]
-    param2 = 'R' + str(instruction['param2'])
+def _shr(destination, source1, source2):
+  R[destination] = R[source1] >> R[source2]
+  PC = PC + 1
 
-  elif (instruction['opcode'] == 'addc'):
-    REG[reg_index] = REG[instruction['param1'] - 1] + instruction['param2']
-    param2 = instruction['param2']
+def _br(address):
+  PC = address
 
-  elif (instruction['opcode'] == 'subc'):
-    REG[reg_index] = REG[instruction['param1'] - 1] - instruction['param2']
-    param2 = instruction['param2']
+def _addc(destination, source, val):
+    R[destination] = R[source] + val
+    PC = PC + 1
 
-  return print(
-    '<== INSTRUCTION EXEC START\n' + instruction['opcode'],
-    'R' + str(instruction['param1']) + ',', str(param2) + ',',
-    'R' + str(instruction['param3']), '\n<== INSTRUCTION FETCH END\n'
-  )
+def _subc(destination, source, val):
+    R[destination] = R[source] - val
+    PC = PC + 1
+
+def _and(destination, source1, source2):
+    R[destination] = R[source1] & R[source2]
+    PC = PC + 1
+
+def _or(destination,  source1,  source2):
+    R[destination] = R[source1] | R[source2]
+    PC = PC + 1
+
+def _xor(destination,  source1,  source2):
+    R[destination] = R[source1] ^ R[source2]
+    PC = PC + 1
+
+def _slt(destination,  source1,  source2):
+  if (R[source1] - R[source2] < 0):
+    R[destination] = 1
+  else:
+    R[destination] = 0
+
+  PC = PC + 1
+
+def _seq(destination,  source1,  source2):
+  if (R[source1] == R[source2]):
+    R[destination] = 1
+  else:
+    R[destination] = 0
+
+  PC = PC + 1
+
+def binary(string):
+  return bin(int(string))[2:]
 
 def main():
-  # Program Counter
-  PC = 0
-
   print('machine started')
 
-  for i, instruction in enumerate(MAIN_MEMORY):
-    # for loop applies INSTRUCTION FETCH
-    decoded_instruction = instructionDecode(instruction)
-    instructionExec(decoded_instruction)
-    printRegisters()
+  R1 = 0
+  R2 = 0
+  R3 = 0
+  
+  while(PC < len(M)):
+    # Fetch binary instruction opcode
+    opcode = binary(M[PC] >> OP_OFFSET)
 
-    PC = PC + 4
-    print('iteration', i, 'done')
+    if (opcode == OP_LD):
+      R1 = binary(M[PC] >> R1_OFFSET)
+      R2 = binary(M[PC] >> R2_OFFSET)
 
+      print("ld R" + int(R1, 2) + ", R" + int(R2, 2))
+      _load(R1, R2)
+
+    elif (opcode == OP_ST):
+      R1 = binary(M[PC] >> R1_OFFSET)
+      R2 = binary(M[PC] >> R2_OFFSET)
+
+      print("st R" + int(R1, 2) + ", R" + int(R2, 2))
+      _store(R1, R2)
+
+    elif (opcode == OP_ADD):
+      R1 = binary(M[PC] >> R1_OFFSET)
+      R2 = binary(M[PC] >> R2_OFFSET)
+      R3 = binary(M[PC] >> R3_OFFSET)
+
+      print("add R" + int(R1, 2) + ", R" + int(R2, 2) + ", R" + int(R3, 2))
+      _add(R1, R2, R3)
+
+    elif (opcode == OP_SUB):
+      R1 = binary(M[PC] >> R1_OFFSET)
+      R2 = binary(M[PC] >> R2_OFFSET)
+      R3 = binary(M[PC] >> R3_OFFSET)
+
+      print("sub R" + int(R1, 2) + ", R" + int(R2, 2) + ", R" + int(R3, 2))
+      _sub(R1, R2, R3)
+
+    elif (opcode == OP_BEQ):
+      R1 = binary(M[PC] >> R1_OFFSET)
+      R2 = binary(M[PC] >> R2_OFFSET)
+      R3 = binary(M[PC] >> R3_OFFSET)
+
+      print("beq R" + int(R1, 2) + ", R" + int(R2, 2) + ", R" + int(R3, 2))
+      _beq(R1, R2, R3)
+
+    elif (opcode == OP_SHL):
+      R1 = binary(M[PC] >> R1_OFFSET)
+      R2 = binary(M[PC] >> R2_OFFSET)
+      R3 = binary(M[PC] >> R3_OFFSET)
+
+      print("shl R" + int(R1, 2) + ", R" + int(R2, 2) + ", R" + int(R3, 2))
+      _shl(R1, R2, R3)
+
+    elif (opcode == OP_SHR):
+      R1 = binary(M[PC] >> R1_OFFSET)
+      R2 = binary(M[PC] >> R2_OFFSET)
+      R3 = binary(M[PC] >> R3_OFFSET)
+
+      print("shr R" + int(R1, 2) + ", R" + int(R2, 2) + ", R" + int(R3, 2))
+      _shr(R1, R2, R3)
+
+    elif (opcode == OP_BR):
+      R1 = binary(M[PC] >> R1_OFFSET)
+
+      print("br R" + int(R1, 2))
+      _br(R1)
+
+    elif (opcode == OP_ADDC):
+      R1 = binary(M[PC] >> R1_OFFSET)
+      R2 = binary(M[PC] >> R2_OFFSET)
+      R3 = binary(M[PC] >> R3_OFFSET)
+
+      print("addc R" + int(R1, 2) + ", R" + int(R2, 2) + ", R" + int(R3, 2))
+      _addc(R1, R2, R3)
+
+    elif (opcode == OP_SUBC):
+      R1 = binary(M[PC] >> R1_OFFSET)
+      R2 = binary(M[PC] >> R2_OFFSET)
+      R3 = binary(M[PC] >> R3_OFFSET)
+
+      print("subc R" + int(R1, 2) + ", R" + int(R2, 2) + ", R" + int(R3, 2))
+      _subc(R1, R2, R3)
+
+    elif (opcode == OP_AND):
+      R1 = binary(M[PC] >> R1_OFFSET)
+      R2 = binary(M[PC] >> R2_OFFSET)
+      R3 = binary(M[PC] >> R3_OFFSET)
+
+      print("and R" + int(R1, 2) + ", R" + int(R2, 2) + ", R" + int(R3, 2))
+      _and(R1, R2, R3)
+
+    elif (opcode == OP_OR):
+      R1 = binary(M[PC] >> R1_OFFSET)
+      R2 = binary(M[PC] >> R2_OFFSET)
+      R3 = binary(M[PC] >> R3_OFFSET)
+
+      print("or R" + int(R1, 2) + ", R" + int(R2, 2) + ", R" + int(R3, 2))
+      _or(R1, R2, R3)
+
+    elif (opcode == OP_XOR):
+      R1 = binary(M[PC] >> R1_OFFSET)
+      R2 = binary(M[PC] >> R2_OFFSET)
+      R3 = binary(M[PC] >> R3_OFFSET)
+
+      print("xor R" + int(R1, 2) + ", R" + int(R2, 2) + ", R" + int(R3, 2))
+      _xor(R1, R2, R3)
+
+    elif (opcode == OP_SLT):
+      R1 = binary(M[PC] >> R1_OFFSET)
+      R2 = binary(M[PC] >> R2_OFFSET)
+      R3 = binary(M[PC] >> R3_OFFSET)
+
+      print("slt R" + int(R1, 2) + ", R" + int(R2, 2) + ", R" + int(R3, 2))
+      _slt(R1, R2, R3)
+
+    elif (opcode == OP_SEQ):
+      R1 = binary(M[PC] >> R1_OFFSET)
+      R2 = binary(M[PC] >> R2_OFFSET)
+      R3 = binary(M[PC] >> R3_OFFSET)
+
+      print("seq R" + int(R1, 2) + ", R" + int(R2, 2) + ", R" + int(R3, 2))
+      _seq(R1, R2, R3)
+  
   print('bye')
+  return (0)
 
 if __name__ == '__main__':
   main()
